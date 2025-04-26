4124688f2b78f731d5197c586755f14217d1dc5c
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-26 16:00:18 +0200

    generateMetadata for page SigleCategoryPage

--- app/(pages)/categories/[catName]/page.tsx
+++ app/(pages)/categories/[catName]/page.tsx
@@ -1,9 +1,24 @@
+import { Suspense } from "react";
+import { Metadata } from "next";
+
 import GoBack from "@/app/components/ui/GoBack";
 import CatProductsList from "@/app/features/products/CatProductsList";
+import Loader from "@/app/components/ui/Loader";
+
 import { formatCategoryName } from "@/app/lib/utils";
-import React from "react";
 
 type Params = Promise<{ catName: string }>;
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { catName } = await params;
+  return {
+    title: formatCategoryName(catName),
+  };
+}
 
 const SigleCategoryPage = async ({ params }: { params: Params }) => {
   const { catName } = await params;
@@ -12,7 +27,9 @@ const SigleCategoryPage = async ({ params }: { params: Params }) => {
     <>
       <GoBack />
       <h2>{formatCategoryName(catName)}</h2>
-      <CatProductsList />
+      <Suspense fallback={<Loader />}>
+        <CatProductsList category={catName} />
+      </Suspense>
     </>
   );
 };
