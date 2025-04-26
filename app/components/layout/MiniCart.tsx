48341064037eb4d27c59bcd112fe03e80ecbd8e9
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-26 17:42:32 +0200

    show number of items in MiniCart

--- app/components/layout/MiniCart.tsx
+++ app/components/layout/MiniCart.tsx
@@ -1,11 +1,20 @@
+"use client";
+import { useCart } from "@/app/context/CartContext";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
+import styles from "./_styles/MiniCart.module.css";
 
 const MiniCart = () => {
+  const { totalCount, hydrated } = useCart();
+
   return (
-    <Link href="/cart">
+    <Link href="/cart" className={styles["mini-cart"]}>
       <Image src="/shopping-cart.svg" alt="Cart" width={25} height={25} />
+
+      <span className={styles["total"]}>
+        {hydrated && totalCount > 0 && totalCount}
+      </span>
     </Link>
   );
 };
