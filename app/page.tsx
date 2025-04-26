de41332088033b6c16b4b0af7502120f38b148be
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-26 14:09:34 +0200

    Loader component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import CategoryList from "./features/categories/CategoryList";
+import Loader from "./components/ui/Loader";
 
 export default function Home() {
   return (
     <>
       <h2>Product categories</h2>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<Loader />}>
         <CategoryList />
       </Suspense>
     </>
