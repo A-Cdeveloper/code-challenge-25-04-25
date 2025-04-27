93071938cd652c92241d5f637a727f4ffd3b17e8
Aleksandar <aleksandar@e-vlasotince.info>
2025-04-27 05:11:18 +0200

    DeleteIcon

---
+++ app/components/ui/DeleteIcon.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./DeleteIcon.module.css";
+
+const DeleteIcon = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <div className={styles["delete-icon"]} onClick={onClick}>
+      <Image src="/trash.svg" alt="Delete" width={16} height={16} />
+    </div>
+  );
+};
+
+export default DeleteIcon;
